f8a0395361b6e76137f87e82d4ab7374b7c79d0f
John Adams <jna@retina.net>
2020-05-20 22:50:31 -0700

    nearly a full rewrite of the original. check in prior work

--- index.js
+++ index.js
@@ -5,20 +5,56 @@ const socketio = require('socket.io')
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
+const log = require('npmlog-ts')
 
-const title = 'Buffer Buzzer'
+const title = 'Dirty Talk Game Show'
+const timeStep = 5;
 
+// module configuration
+app.disable('x-powered-by');
+log.timestamp = true
+
+// zero out stats
+var statObj = { 'connections' : 0,
+		'connected' : 0,
+		'invalid_hash' : 0,
+		'msg_join' : 0,
+		'msg_clear' : 0,
+		'msg_buzz' : 0,
+		'msg_subscribe' : 0,
+		'msg_unsubscribe' : 0,
+		'msg_disconnect' : 0,
+		'msg_chat' : 0,
+		'noauth' : 0,
+		'uptime' : 0,
+		'valid_hash' : 0 };
+
+// trap exceptions. Safety 3rd! We'll abort after this.
+process.on('uncaughtException', function (err) {
+  console.error('uncaughtException:', err.message)
+  console.error(err.stack)
+  process.exit(1)})
+
+// this map stores all user and gameplay data
 let data = {
-  users: new Set(),
-  buzzes: new Set(),
+  users: new Map(),
+  scores: new Map(), // userid -> score mapping
+  lastbuzz: undefined, // last known buzzed in
+  timeRemain: 60,
+  clockRunning: false,
+  lockout: true
 }
 
+// we start with a default of 60 seconds
+// but this can be changed in game.
+let maxTime = 60;
+
 const getData = () => ({
+  timeRemain: data.timeRemain,
+  clockRunning: data.clockRunning,
+  lockout: data.lockout,
   users: [...data.users],
-  buzzes: [...data.buzzes].map(b => {
-    const [ name, team ] = b.split('-')
-    return { name, team }
-  })
+  lastbuzz: data.lastbuzz
 })
 
 app.use(express.static('public'))
@@ -26,25 +62,145 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => res.render('index', { title }))
 app.get('/host', (req, res) => res.render('host', Object.assign({ title }, getData())))
+app.get('/healthcheck', function(req, res){
+  res.send('OK');
+});
+app.get('/status', function(req, res){
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(statObj) + "\n");
+});
 
 io.on('connection', (socket) => {
+  var clientIp = socket.request.connection.remoteAddress;
+  log.info(`clientIP - (socket ${socket.id}) connected`);
+
+  statObj.connections++;
+  statObj.connected++;
+
+  // these operations are coming from the socket to the
+  // service
   socket.on('join', (user) => {
-    data.users.add(user.id)
-    io.emit('active', [...data.users].length)
-    console.log(`${user.name} joined!`)
+    statObj.msg_join++;
+
+    data.users.set(socket.id, user);
+    data.scores.set(socket.id, 0);
+
+    // it's not possible to transmit maps so we have to marshall them into a string, and then array...
+    let transitString = JSON.stringify(Array.from(data.users));
+    io.emit('active', transitString);
+    log.info(`${socket.id}: ${user.name} joined!`)
+
+    // update this user on the current world-state
+    socket.emit('lockout', data.lockout);
+    socket.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
   })
 
   socket.on('buzz', (user) => {
-    data.buzzes.add(`${user.name}-${user.team}`)
-    io.emit('buzzes', [...data.buzzes])
-    console.log(`${user.name} buzzed in!`)
+    statObj.msg_buzz++;
+    
+    // lock everyone out and stop the clock
+    data.lockout = true;
+    data.clockRunning = false;
+
+    io.emit('lockout', data.lockout);
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+
+    data.lastbuzz = socket.id;
+    io.emit('lastbuzz', { id: data.lastbuzz, name: data.users.get(socket.id).name });
+    log.info(`${socket.id}: ${data.users.get(socket.id).name} buzzed in!`);
   })
 
   socket.on('clear', () => {
-    data.buzzes = new Set()
-    io.emit('buzzes', [...data.buzzes])
-    console.log(`Clear buzzes`)
+    statObj.msg_clear++;
+
+    data.lastbuzz = undefined;
+    io.emit('lastbuzz', data.lastbuzz)
+    log.info(`Clear lastbuzz`)
   })
+
+  socket.on('resetclock', () => {
+    statObj.msg_resetclock++;
+
+    data.timeRemain = maxTime;
+    data.clockRunning = false;
+    data.lockout = true;
+    io.emit('lockout', data.lockout);
+
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+    log.info(`Reset Clock`)
+  });
+
+  socket.on('startclock', () => {
+    statObj.msg_startclock++;
+
+    if (data.timeRemain > 0) {
+      data.clockRunning = true;
+      data.lockout = false;
+      io.emit('lockout', data.lockout);
+    }
+
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+    log.info(`Start clock with ${data.timeRemain}`);
+  });
+ 
+  socket.on('addtime', () => {
+    statObj.msg_addtime++;
+    data.timeRemain = data.timeRemain + timeStep;
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+    log.info(`Add time (+${timeStep}) - now ${data.timeRemain}`);
+  });
+
+  socket.on('subtime', () => {
+    statObj.msg_subtime++;
+
+    data.timeRemain = data.timeRemain - timeStep;
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+    log.info(`Subtract time (-${timeStep}) - now ${data.timeRemain}`);
+  });
+
+  socket.on('pauseclock', () => {
+    statObj.msg_stopclock++;
+    data.clockRunning = false;
+    data.lockout = true;
+    io.emit('lockout', data.lockout);
+    io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+    log.info(`Pause clock with ${data.timeRemain}`);
+  })
+
+  socket.on('disconnect', function () {
+    // user disconnected, remove them from our state
+    statObj.connected--;
+    statObj.msg_disconnect++;
+    data.users.delete(socket.id);
+    data.scores.delete(socket.id);
+    
+    let userTransit = JSON.stringify(Array.from(data.users));
+    io.emit('active', userTransit);
+
+    let scoreTransit = JSON.stringify(Array.from(data.scores));
+    io.emit('scores', scoreTransit);
+
+    log.info(`${socket.request.connection.remoteAddress} - (socket ${socket.id}) connected`);
+  });
+
+
 })
 
-server.listen(8090, () => console.log('Listening on 8090'))
+function fireTick() { 
+  if (data.clockRunning) {
+    data.timeRemain = data.timeRemain - 1;
+    if (data.timeRemain <= 0) {
+      data.timeRemain = 0;
+      data.clockRunning = false;
+      data.lockout = true;
+      io.emit('lockout', data.lockout);
+      io.emit('timesup',true);
+    }
+  }
+  io.emit('tick', { timeRemain: data.timeRemain, clockRunning: data.clockRunning });
+}
+
+// set up our clock routine, which will run forever.
+const timer = setInterval(fireTick, 1000);
+
+server.listen(8090, () => log.info('Listening on 8090'))
--- public/join.js
+++ public/join.js
@@ -5,43 +5,70 @@ const joined = document.querySelector('.js-joined')
 const buzzer = document.querySelector('.js-buzzer')
 const joinedInfo = document.querySelector('.js-joined-info')
 const editInfo = document.querySelector('.js-edit')
+const clock = document.querySelector('.js-clock')
+const lastbuzz = document.querySelector('.js-lastbuzz');
 
-let user = {}
+let user = {};
 
 const getUserInfo = () => {
-  user = JSON.parse(localStorage.getItem('user')) || {}
+  user = JSON.parse(sessionStorage.getItem('user')) || {}
   if (user.name) {
     form.querySelector('[name=name]').value = user.name
-    form.querySelector('[name=team]').value = user.team
   }
-}
+  if (user.team) {
+    form.querySelector('[name=team]').value = user.team 
+  }
+};
+
 const saveUserInfo = () => {
-  localStorage.setItem('user', JSON.stringify(user))
-}
+  sessionStorage.setItem('user', JSON.stringify(user))
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  user.name = form.querySelector('[name=name]').value
-  user.team = form.querySelector('[name=team]').value
-  if (!user.id) {
-    user.id = Math.floor(Math.random() * new Date())
-  }
-  socket.emit('join', user)
-  saveUserInfo()
-  joinedInfo.innerText = `${user.name} on Team ${user.team}`
-  form.classList.add('hidden')
-  joined.classList.remove('hidden')
-  body.classList.add('buzzer-mode')
-})
+  user.name = form.querySelector('[name=name]').value;
+//  user.team = form.querySelector('[name=team]').value;
+
+  socket.emit('join', user);
+  saveUserInfo();
+  joinedInfo.innerText = `${user.name}`;
+  form.classList.add('hidden');
+  joined.classList.remove('hidden');
+  body.classList.add('buzzer-mode');
+});
 
 buzzer.addEventListener('click', (e) => {
-  socket.emit('buzz', user)
-})
+  socket.emit('buzz', user);
+});
+
+socket.on('tick', (data) => {
+  const min = `${Math.floor(data.timeRemain / 60)}`;
+  const sec = `${data.timeRemain - (min*60)}`;
+
+  clock.innerText = `${min.padStart(2,'0')}:${sec.padStart(2,'0')}`; 
+});
+
+socket.on('lockout', (data) => {
+  if (data) {
+    buzzer.disabled = true;
+  } else {
+    buzzer.disabled = false;
+  }
+});
+
+socket.on('lastbuzz', (buzz) => {
+  if (buzz !== null) {
+    lastbuzz.innerHTML = `${buzz.name} buzzed in first!`;
+  } else {
+    // this is a clear request
+    lastbuzz.innerHTML = '';
+  }
+});
 
 editInfo.addEventListener('click', () => {
   joined.classList.add('hidden')
   form.classList.remove('hidden')
   body.classList.remove('buzzer-mode')
-})
+});
 
-getUserInfo()
+getUserInfo();
