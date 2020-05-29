ed8cb2657c13d387f00018c118025efbf5c707c5
John Adams <jna@retina.net>
2020-05-28 23:26:05 -0700

    add proper env handling

--- packages/gobuzzyourself-web/src/App.js
+++ packages/gobuzzyourself-web/src/App.js
@@ -41,7 +41,7 @@ library.add(
   faCircle
 );
 // API server
-const ENDPOINT=process.env.API_HOST ? process.env.API_HOST : "http://localhost:8090";
+const ENDPOINT=process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : "http://localhost:8090";
 
 // set up our heartbeat
 function App() {
