fde9e36f061ec9b2e98d3ae5a0befb080e5d495d
John Adams <jna@retina.net>
2020-06-14 03:10:57 -0700

    give privacy page the button too

--- packages/gobuzzyourself-web/src/pages/PrivacyPage/index.js
+++ packages/gobuzzyourself-web/src/pages/PrivacyPage/index.js
@@ -1,10 +1,11 @@
 import NavBar from "../../components/NavBar";
 import DTHeader from "../../components/DTHeader";
-
+import { useHistory } from 'react-router-dom';
 import React from "react";
 
 import {
   Alert,
+  Button,
   Card,
   CardBody,
   CardTitle,
@@ -14,6 +15,14 @@ import {
 } from "reactstrap";
 
 const PrivacyPage = (props) => {
+
+  const history = useHistory();
+
+  const handleButton = () =>{ 
+    let path = '/'; 
+    history.push(path);
+  }
+
   let sockErrorComp = null;
   if (props.socketError != null) {
     sockErrorComp = (<Alert color="danger">{props.socketError}</Alert>);
@@ -37,7 +46,8 @@ const PrivacyPage = (props) => {
             <CardTitle><h1>Privacy &amp; Security</h1></CardTitle>
             <hr/>
             <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          </CardBody>
+            <Button color="primary" className="px-4" onClick={handleButton  } > Go back </Button>
+         </CardBody>
         </Card>
         </Col>
       </Row>
